999a7a1063e4c1a3447351c5e317fa54ea95f991
unknown <istartgame31@gmail.com>
2026-03-21 16:57:20 +0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod select_source;
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+
+    }
+}
---
+++ src/select_source/mod.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct InputPayload{
+    pub id: String,
+    pub title: String,
+    pub source: String
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct OuputPayloadInfo{
+    pub id: String,
+    pub title: String
+}
