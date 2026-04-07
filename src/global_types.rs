eb3d2ba8d186ceb6d22dcff41ca24d6b3364b40a
unknown <istartgame31@gmail.com>
2026-04-08 01:14:27 +0700

    fix struct

--- src/global_types.rs
+++ src/global_types.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(rename_all = "lowercase")]
 pub enum Source{
     Anime,
